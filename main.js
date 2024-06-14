166788775c1715763b4cae1d72a72a4c9f71cc3a
nathanalves007 <110548631+nathanalves007@users.noreply.github.com>
2024-06-14 09:42:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo")
 const tempo = document.querySelectorAll(".contador");
+tempo[0].textContent = "Olá";
+const tempoObjetivo1 = new Date (2007,06);
+const tempoObjetivo2 = new Date (2024,06);
+const agora = new Date ();
+tempo[1]. textContent = agora
 console.log(tempo);
 for(let i = 0; i < botoes.length ; i++){
     botoes[i].onclick = function(){
