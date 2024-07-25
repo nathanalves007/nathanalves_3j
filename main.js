9febacc882f7b220046368527568f90a48014099
nathanalves007 <110548631+nathanalves007@users.noreply.github.com>
2024-07-25 08:08:28 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -13,10 +13,20 @@ let horas;
 horas = minutos/60
 let dias;
 dias = horas/24
-segundos = Math.floor(segundos);
-minutos = Math.floor(minutos);
-horas = Math.floor(horas);
-dias = Math.floor(dias);
+segundos = Math.floor(tempoObjetivo-agora)/1000);
+minutos = Math.floor(segundos/60);
+horas = Math.floor(minutos/60);
+dias = Math.floor(horas/24);
+
+segundos = segundos%60
+minutos = minutos%60
+horas = horas%24
+
+if (segundos >= 0){
+  return `faltam ${dias} dias, ${horas} horas, ${minutos} minutos, ${segundos} segundos`
+} else {
+  return 'Prazo Finalizado!'
+}
 tempo[0].textContent = `faltam ${dias} dias, ${horas} horas, ${minutos} minutos, ${segundos} segundos`
 tempo[1].  textContent = agora;
 for (let i = 0; i < botoes.length ; i++){
